163bb99f4325c01299affc80e690536c9006854e
Simba Galaxy <galaxy001@gmail.com>
2012-03-06 02:40:08 +0800

    new file:   baseCallingMatrix/autobam.pl

---
+++ src/stator/baseCallingMatrix/autobam.pl
@@ -0,0 +1,31 @@
+#!/bin/env perl
+use strict;
+use warnings;
+
+my $SAMTOOLSBIN="/ifs1/ST_ASMB/USER/yuanjy/huxuesong/tmp/group/rev/test/samtools";
+my $CALTUATORBIN="/ifs1/ST_ASMB/USER/yuanjy/huxuesong/tmp/group/rev/test/pl/baseCallingMatrix/baseCalling_Matrix_calculator.pl";
+my $MAXREADStoCHECK=10000;
+
+die "Usage: $0 <single_bamfile> <output_prefix> <other_options>\n" if @ARGV<3;
+my $name=shift;
+my $out=shift;
+my ($READLEN,$lines)=(0,0);
+open IN,'-|',"$SAMTOOLSBIN view -f 3 -F 1536 $name" or die "Error opening $name : $!\n";
+
+while (<IN>) {
+	next if /^@\w\w\t\w\w:/;
+	chomp;
+	my @read1=split /\t/;
+	next unless ($read1[1] & 3) == 3;  # paired + mapped in a proper pair
+	next if $read1[1] >= 256;   # not primary || QC failure || optical or PCR duplicate
+	next unless $read1[5] =~ /^(\d+)M$/;
+	$READLEN = $1 if $READLEN < $1;
+	++$lines;
+	last if $lines > $MAXREADStoCHECK;
+}
+close IN;
+
+my $CLI="$SAMTOOLSBIN view -f 3 -F 1792 $name | $CALTUATORBIN -bp sam -l $READLEN @ARGV -o $out >${out}.log 2>${out}.err";
+print "Read_Length of [$name]:\n$READLEN\n\nCLI:[$CLI]\n";
+
+system $CLI;
