d68db25c8d7d8e1f99ce55733c096970b091d090
Yuuki Galaxy <galaxy001@gmail.com>
2014-10-12 16:16:51 +0800

    modified:   srclean.sh,cleansrc.lst

--- src/srclean.sh
+++ src/srclean.sh
@@ -3,7 +3,7 @@
 rm *~ */*~
 rm config.log config.status Makefile.in ./pirs/Makefile.in
 
-rm Makefile depcomp configure missing aclocal.m4 INSTALL install-sh stamp-h1
+rm Makefile ./pirs/Makefile depcomp configure missing aclocal.m4 INSTALL install-sh stamp-h1
 
 rm -fr ./pirs/.deps ./autom4te.cache
 
